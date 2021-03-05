25b6e61a4a17b427b757665fad62c659c2ca4440
kostidik <48972779+dikaevkostya1@users.noreply.github.com>
2021-03-05 11:44:45 +0700

    1

---
+++ resources/views/ajax/request/request.blade.php
@@ -0,0 +1,13 @@
+<form method="post">
+    @csrf
+    <input type="file">
+    <input type="text" placeholder="Дата рождения">
+    <input type="text" placeholder="Адрес проживания">
+        <input type="text" placeholder="Номер телефона">
+        <input type="text" placeholder="Год окончания">
+        <input type="text" placeholder="Логин">
+        <input type="text" placeholder="E-mail">
+        <input type="password" placeholder="Пароль">
+        <input type="password" placeholder="Подтверждение пароля">
+        <input type="submit" value="Продолжить">
+</form>
